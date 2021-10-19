a11fb77d1614a1c9e6f5c4e18ecbd1a764651d50
sjiamnocna <jancikuvmail@gmail.com>
2021-10-19 15:08:12 +0200

    Rebuilt API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createLogicalOr } from 'typescript';
 import './App.css';
-import callApi from './helper/api/api';
-
-const click = e => {
-  callApi('user', 'login', {
-    username: "ahoj",
-    password: "12345"
-  })
-  .then(res => res.json())
-  .then(res => console.log(res));
-}
+import APICall from './helper/api/api';
 
 const App = () => {
+  const [userLogged, setUserLogged] = useState(false);
+  const [user, setUser] = useState({});
+
+  
+  
+  const login = e => {
+    APICall({
+      resource: 'user',
+      action: 'login',
+      id: 42,
+      data: {
+        'username': 'ahoj',
+        'password': '12345'
+      }
+    })
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
+  
   return (
     <div className="App">
-      <button className="call-api" onClick={click}>Zavolej</button>
+      <p>{userLogged ? 'Přihlášen' : 'Ne'}</p>
+      <button className="call-api" onClick={login}>Přihlásit se</button>
     </div>
   );
 };
--- src/helper/api/api.js
+++ src/helper/api/api.js
@@ -1,13 +1,36 @@
-const callApi = (resource, action, data) =>
-    fetch(`/api/${resource}/${action}`, {
-        method: 'POST',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(data)
-    }).catch(err => {
-        console.log('catch', err);
-    });
+/**
+ * sjiamnocna
+ * @param object Fetch API parameters
+ *  'resource'  string  (reqired)
+ *  'action'    string  if not set, default action is used
+ *  'id'        int     resource id
+ *  'method'    HTTP2   REST method ['GET', 'PUT', 'DELETE', 'PATCH'] (POST is default)
+ *  'headers'   Object  Key => Value pairs
+ *  'data'      Object  Key => Value pairs, data used to create request body
+ * @returns promise
+ */
+const APICall = (param) => {
+    const {resource, action, id} = param
+    const path = `/api/${resource}` + (action ? `/${action}` : '') + (id ? `/${id}` : '')
 
-export default callApi;
+    // check method
+    const method = ['GET', 'PUT', 'DELETE', 'PATCH'].includes(String(param.method).toUpperCase()) ?
+        String(param.method).toUpperCase() : 'POST'
+
+    // combine headers into one object
+    const defaultHeaders = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'content-type': 'application/json',
+    }
+    const headers = param.headers !== undefined ?
+        Object.assign(param.headers, defaultHeaders) : defaultHeaders; 
+
+    return fetch(path, {
+        method: method,
+        headers: headers,
+        body: method !== 'GET' ?
+            JSON.stringify(param.data) : null
+    })
+}
+
+export default APICall;
