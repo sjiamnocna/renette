4f53ab6a909bd7bcfdac5111892ec04144e1ea60
sjiamnocna <jancikuvmail@gmail.com>
2022-01-10 08:47:35 +0100

    Init, authorize, gethello buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,70 @@
-import { Button, ButtonGroup, Container } from '@mui/material';
-import React, { useState } from 'react';
-import './App.css';
-import CallAPIWithKey from './helper/api/api';
+import { Button, ButtonGroup, Container } from '@mui/material'
+import React from 'react'
+import './App.css'
+import CallAPI from './helper/api/api'
 
-const App = () => {
-  const [hello, setHello] = useState('');
-  console.log(CallAPIWithKey)
-  const getFortyTwo = e => {
-    CallAPIWithKey({
-      resource: 'hello'
-    })
-      .then(res => res.json())
-      .then(res => setHello(res.hello))
+/**
+ * API class
+ * - Request Authentication
+ * - Authorization with service name and key
+ * - GET/POST... requests
+ */
+const App = class extends React.PureComponent {
+  state = {
+    hello: null,
+    apiReady: false,
+    apiAuthorized: false
   }
-  return (
-    <div>
-      <Container>
-        <ButtonGroup>
-          <Button onClick={getFortyTwo}>
-            Get hello
-          </Button>
-          <div>
-            {hello}
-          </div>
-        </ButtonGroup>
-      </Container>
-    </div>
-  );
-};
 
-export default App;
+  render() {
+    const APIinit = e => {
+      this.setState({ hello: 'Authenticating request...' })
+      CallAPI.api_init(res => {
+        if (res) {
+          this.setState({ apiReady: true })
+        } else {
+          this.setState({ error: true })
+        }
+      }).then(res => {
+        console.log(res)
+        if (typeof res === 'string' && res.length === 32) {
+          this.setState({
+            apiReady: true,
+            hello: 'Done'
+          })
+        }
+      })
+    }
+
+    const APIAuth = e => {
+      this.setState({hello: 'Authorizing service'})
+      CallAPI.api_authorize().then(res => {
+        this.setState({hello: 'Authorized! Congrats'})
+        console.log(res)
+      })
+    }
+
+    return (
+      <div>
+        <Container className='mt-3'>
+          <ButtonGroup>
+            <Button onClick={APIinit} disabled={this.state.apiReady}>
+              Init
+            </Button>
+            <Button onClick={APIAuth} disabled={!this.state.apiReady && !this.state.apiAuthorized}>
+              Authorize
+            </Button>
+            <Button onClick={() => this.setState({ hello: 'Hello world!' })}>
+              Get hello
+            </Button>
+            <div className=''>
+              {this.state.hello}
+            </div>
+          </ButtonGroup>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/helper/api/api.js
+++ src/helper/api/api.js
@@ -1,9 +1,6 @@
-import { access } from "fs"
-
 const credentials = {
     serviceName: 'testing',
-    serviceKey: 'abcdef',
-    accessKey: null
+    serviceKey: 'abcdef'
 }
 
 // combine headers into one object
@@ -12,67 +9,135 @@ const defaultHeaders = {
     'content-type': 'application/json',
 }
 
-/**
- * HOC - first access to API and get generated hash
- * @param {*} baseUri 
- * @param {*} serviceName 
- * @param {*} serviceKey
- * @returns 
- */
-const CallAPIWithKey = async (baseUri, serviceName, serviceKey) => {
-    // first time we need API
-    const accessDetails = {}
-    
-    fetch(baseUri + '/ch/init', {
-        method: 'POST',
-        headers: {
-            'X-Service-Name': serviceName,
-            ...defaultHeaders
-        }
-    }).then(res => res.text()).then(res => {
-        const accessToken = res
-        console.log('acc', accessToken)
+const allowedMethod = ['POST', 'GET', 'PUT', 'DELETE']
+const apiUri = '/api'
 
-        return fetch(baseUri + '/ch/init', {
-            method: 'POST',
-            headers: {
-                'X-Access-Key': accessToken,
-                'X-Service-Key': credentials.serviceKey,
-                ...defaultHeaders
-            }
-        }).then(res => accessDetails.accessKey = res.text())
-    })
-
+class CAPI
+{
+    /** @var string Access key to use to authenticate each request */
+    accessKey
+    /** @var bool If the API is authorized via key or not */
+    authorized
+    /** @var bool API lock */
+    lock
+    /** @var string API Base URI */
+    baseUri
 
     /**
-     * 
-     * @param object Fetch API parameters
-     *  'resource'  string  (reqired)
-     *  'action'    string  if not set, default action is used
-     *  'id'        int     resource id
-     *  'method'    HTTP2   REST method ['GET', 'PUT', 'DELETE', 'PATCH'] (POST is default)
-     *  'headers'   Object  Key => Value pairs
-     *  'data'      Object  Key => Value pairs, data used to create request body
-     * @returns promise
+     * Authenticate session
+     * @param function Function after the work is done
      */
-    return (param) => {
-        const {resource, action, id, method} = param
-        const path = `${baseUri}/${resource}` + (action ? `/${action}` : '') + (id ? `/${id}` : '')
-    
-        const fetchParam = {
-            method: method ?? 'POST',
-            headers: param.hasOwnProperty('header') ?
-                // create new object from both default headers and param.headers
-                Object.assign({
-                    'x-access-key' : accessDetails.accessKey,
-                    ...defaultHeaders
-                }, param.headers) : defaultHeaders,
-            body: method !== 'GET' ?
-                JSON.stringify(param.data) : null
+    async api_init(onReady)
+    {
+        if (this.accessKey || this.lock) {
+            // if the API is authenticated, then do nothing
+            return false
         }
 
-        return fetch(path, fetchParam)
+        // prevent multiple calls
+        this.lock = true
+        return await this.get({
+            resource: 'Ch',
+            action: 'init',
+            headers: {
+                'X-Service-Name': credentials.serviceName
+            }
+        })
+        .then(res => res.text())
+        .then(res => {
+            this.lock = false
+            if (typeof onReady === 'function'){
+                // callback after init actions
+                onReady(typeof res === 'string')
+            }
+            return res ?
+                this.accessKey = res : null
+        })
     }
+
+    async api_authorize(onReady)
+    {
+        if (this.authorized || this.lock) {
+            // if the API is locked, then do nothing
+            return false
+        }
+
+        // prevent multiple calls
+        this.lock = true
+        return await this.get({
+            resource: 'Ch',
+            action: 'authorize',
+            headers: {
+                'X-Access-Key': this.accessKey,
+                'X-Service-Key': credentials.serviceKey,
+            }
+        })
+        .then(res => res.text())
+        .then(res => {
+            this.lock = false
+            if (typeof onReady === 'function') {
+                // callback after init actions
+                onReady(typeof res === 'string')
+            }
+            if (typeof res === 'string' && res.length === 32){
+                return this.accessKey = res;
+            }
+            console.log(res)
+        })
+    }
+
+    async get(param)
+    {
+        param.method = 'GET'
+
+        if (param.data){
+            // cannot use data with GET method
+            throw Error('Cant use data with GET method')
+        }
+
+        return this.__fetch(param)
+    }
+
+    async post(param)
+    {
+        param.method = 'POST'
+
+        return this.__fetch(param)
+    }
+
+    /**
+    * 
+    * @param object Fetch API data
+    *  'resource'  string  (reqired)
+    *  'action'    string  if not set, default action is used
+    *  'id'        int     resource id
+    *  'method'    HTTP2   REST method ['GET', 'PUT', 'DELETE', 'PATCH'] (POST is default)
+    *  'headers'   Object  Key => Value pairs
+    *  'data'      Object  Key => Value pairs, data used to create request body
+    * @returns promise
+    */
+    async __fetch(param)
+    {
+       const {resource, action, id} = param
+       const method = allowedMethod.includes(param.method) ? param.method : 'GET'
+       const path = `${apiUri}/${resource}` + (action ? `/${action}` : '') + (id ? `/${id}` : '')
+   
+       const fetchParam = {
+           method: method,
+           headers:
+               // create new object from both default headers and param.headers
+               {
+                   ...defaultHeaders,
+                   ...param.headers
+               },
+           body: method !== 'GET' ? // if request is GET, body is not allowed
+                JSON.stringify(param.data) : null
+       }
+
+       return fetch(path, fetchParam)
+   }
 }
 
-export default CallAPIWithKey('/api', credentials.serviceName, credentials.serviceKey);
+const API = new CAPI()
+
+export default API
